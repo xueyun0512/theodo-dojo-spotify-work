c980b95cb9ad3d06dad777f4e28444421ce6f055
xueyun0512 <131191590+xueyun0512@users.noreply.github.com>
2023-04-18 19:28:58 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import logo from './assets/logo.svg';
 import './App.css';
 
+let trackIndex = 0;
+
+const goToNextTrack = () => {
+  trackIndex += 1;
+}
+
+<audio src={trackUrls[trackIndex]} autoPlay controls />
+<button onClick={goToNextTrack}>
+    Next track
+</button>
+
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
   'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
